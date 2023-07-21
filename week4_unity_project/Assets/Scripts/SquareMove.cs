0f633634bb3b10a8eeae79c6c85e9671503d9d84
Mo Jihun <jihunmo@kaist.ac.kr>
2023-07-21 10:52:41 +0900

    square move script

---
+++ week4_unity_project/Assets/Scripts/SquareMove.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareMove : MonoBehaviour
+{
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+
+    [SerializeField]
+    private float moveSpeed = 5f;
+
+    private float jumpForce = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+
+        // Jump        
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        // move
+        Vector3 move = new Vector3(moveX, 0, 0);
+        transform.position += move * moveSpeed * Time.deltaTime;
+
+        // flip
+        if (moveX > 0)
+        {
+            sr.flipX = false;
+        }
+        else if (moveX < 0)
+        {
+            sr.flipX = true;
+        }
+    }
+}
