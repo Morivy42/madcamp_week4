6a46c5b9e6f8d766a1f7db22b39e6c54fc12c1f0
fairykhy <fairykhy02@gmail.com>
2023-07-27 19:20:55 +0900

    hy afinal 230727 19:20

---
+++ week4_unity_project/Assets/Scripts/EndingControl.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingControl : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        // 애니메이션이 끝났는지 확인합니다.
+        if (stateInfo.normalizedTime >= 0.99f)
+        {
+            SceneManager.LoadScene("OpeningScene");
+        }
+    }
+}
