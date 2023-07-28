ad26bd833acd512d445eb95a14e05d2a8be336b3
Mo Jihun <jihunmo@kaist.ac.kr>
2023-07-28 13:37:18 +0900

    수정 최종

--- week4_unity_project/Assets/OpeningScript.cs
+++ week4_unity_project/Assets/OpeningScript.cs
@@ -7,6 +7,10 @@ public class OpeningScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // PlayerPrefs delete all
+        
+        PlayerPrefs.DeleteAll();
+
         // store stage number
         PlayerPrefs.SetInt("stage", 0);
         
